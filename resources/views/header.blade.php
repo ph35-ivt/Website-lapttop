03f2eddbb1435f1a612c9e6ee49864f02d1ddace
quy <dauconquyx10@gmail.com>
2019-11-18 20:01:19 +0700

    init web

---
+++ resources/views/header.blade.php
@@ -0,0 +1,19 @@
+<div class="header">
+		<div class="headertop_desc">
+			<div class="call">
+				 <p><span>Need help?</span> call us <span class="number">0386556593</span></span></p>
+			</div>
+			<div class="account_desc">
+				<ul>
+					<li><a href="#">Register</a></li>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Delivery</a></li>
+					<li><a href="#">Checkout</a></li>
+					<li><a href="#">My Account</a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+		
+	 <div class="clear"></div>
+</div>
