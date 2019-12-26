4ddaec993f6aa2d1accccf12203fe0724fb27f82
quy <dauconquyx10@gmail.com>
2019-12-26 21:11:41 +0700

    edit code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Category;
-use App\OrderDetai;
+use App\OrderDetail;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateProductRequest;
 
@@ -84,7 +84,7 @@ class ProductController extends Controller
     public function update(Request $request, $id)
     {
         $product = Product::find($id);
-        $data= $request->only('name','category_id','product_slug','content','price','link','quantity','status');
+        $data= $request->only('name','category_id','slug','content','price','link','quantity','status');
         if($request->hasFile('link'))
         {
             $link = rand(1,9999).$request->file('link')->getClientOriginalName();        
@@ -104,8 +104,19 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        
+        $listOrder_details = OrderDetail::where('product_id', $id)->get();
+        foreach ($listOrder_details as $detail) {
+            $detail->delete(); 
+        }        
         Product::destroy($id);
         return redirect()->route('list-product')->with('success','Delete product successful !');
+    }
+    public function searchProduct(Request $request){
+        $listProducts = Product::paginate(5);
+        $search = Product::where('name','like','%'.$request->search.'%')
+                    ->orwhere('price',$request->search)
+                    ->get();
+        // dd($search);
+        return view('admin.products.search_product',compact('search','listProducts'));
     }
 }
